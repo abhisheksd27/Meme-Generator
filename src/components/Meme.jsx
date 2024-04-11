8fdedcd597c53a4105d4074ac7f918701ffb2008
Abhishek Shankar <abhisheksdevadiga271@gmail.com>
2024-04-11 21:32:11 +0530

    download

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,74 +1,107 @@
-import React from "react"
+import React from "react";
 
 export default function Meme() {
-    const [meme, setMeme] = React.useState({
-        topText: "",
-        bottomText: "",
-        randomImage: "http://i.imgflip.com/1bij.jpg" 
-    })
-    const [allMemes, setAllMemes] = React.useState([])
-    
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMemes, setAllMemes] = React.useState([]);
 
-    
-    React.useEffect(() => {
-        fetch("https://api.imgflip.com/get_memes")
-            .then(res => res.json())
-            .then(data => setAllMemes(data.data.memes))
-    }, [])
-    
+  React.useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
+
+  function getMemeImage() {
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[randomNumber].url;
+    setMeme((prevMeme) => ({ ...prevMeme, randomImage: url }));
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({ ...prevMeme, [name]: value }));
+  }
+
+  function downloadMeme() {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+  
+    const img = new Image();
+    img.crossOrigin = "anonymous";
+    img.src = meme.randomImage;
+  
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+  
+      ctx.drawImage(img, 0, 0);
+  
      
-    
-    
-    
-    function getMemeImage() {
-        const randomNumber = Math.floor(Math.random() * allMemes.length)
-        const url = allMemes[randomNumber].url
-        setMeme(prevMeme => ({
-            ...prevMeme,
-            randomImage: url
-        }))
-        
-    }
-    
-    function handleChange(event) {
-        const {name, value} = event.target
-        setMeme(prevMeme => ({
-            ...prevMeme,
-            [name]: value
-        }))
-    }
-    
-    return (
-        <main>
-            <div className="form">
-                <input 
-                    type="text"
-                    placeholder="Top text"
-                    className="form--input"
-                    name="topText"
-                    value={meme.topText}
-                    onChange={handleChange}
-                />
-                <input 
-                    type="text"
-                    placeholder="Bottom text"
-                    className="form--input"
-                    name="bottomText"
-                    value={meme.bottomText}
-                    onChange={handleChange}
-                />
-                <button 
-                    className="form--button"
-                    onClick={getMemeImage}
-                >
-                    Get a new meme image 🖼
-                </button>
-            </div>
-            <div className="meme">
-                <img src={meme.randomImage} className="meme--image" />
-                <h2 className="meme--text top">{meme.topText}</h2>
-                <h2 className="meme--text bottom">{meme.bottomText}</h2>
-            </div>
-        </main>
-    )
+      ctx.font = "32px Arial";
+      ctx.fillStyle = "white";
+      ctx.strokeStyle = "black";
+      ctx.lineWidth = 2;
+  
+      const topTextWidth = ctx.measureText(meme.topText).width;
+      const topTextX = (canvas.width - topTextWidth) / 2;
+      ctx.fillText(meme.topText, topTextX, 30);
+      ctx.strokeText(meme.topText, topTextX, 30);
+  
+      
+      const bottomTextWidth = ctx.measureText(meme.bottomText).width;
+      const bottomTextX = (canvas.width - bottomTextWidth) / 2;
+      ctx.fillText(meme.bottomText, bottomTextX, canvas.height - 10);
+      ctx.strokeText(meme.bottomText, bottomTextX, canvas.height - 10);
+  
+      canvas.toBlob((blob) => {
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.download = "meme.png";
+        link.href = url;
+        link.click();
+        URL.revokeObjectURL(url);
+      }, "image/png");
+    };
+  }
+
+  return (
+    <main>
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Top text"
+          className="form--input"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="form--input"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
+        <button className="form--button" onClick={downloadMeme}>
+          Download Meme
+        </button>
+      </div>
+      <div className="meme">
+        <img
+          src={meme.randomImage}
+          className="meme--image"
+          crossOrigin="anonymous"
+        />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
+    </main>
+  );
 }
